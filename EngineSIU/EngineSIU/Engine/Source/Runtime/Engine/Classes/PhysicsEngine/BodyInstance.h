b916400d8568d6949d72e333d60ca940b07d8535
gudtldn <gudtldn1234@gmail.com>
2025-05-24 19:17:20 +0900

    feat: 물리 엔진 관련 BodyInstance와 BodyInstanceCore 클래스 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/PhysicsEngine/BodyInstance.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "BodyInstanceCore.h"
+
+class UPrimitiveComponent;
+
+
+struct FBodyInstance : FBodyInstanceCore
+{
+    DECLARE_STRUCT(FBodyInstance, FBodyInstanceCore)
+
+public:
+    FBodyInstance() = default;
+    virtual ~FBodyInstance() override = default;
+
+    FBodyInstance(const FBodyInstance&) = default;
+    FBodyInstance& operator=(const FBodyInstance&) = default;
+    FBodyInstance(const FBodyInstance&&) = default;
+    FBodyInstance& operator=(FBodyInstance&&) = default;
+
+public:
+    TWeakObjectPtr<UPrimitiveComponent> OwnerComponent;
+};
