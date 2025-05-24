a205a7518cb6883b85ac39efdf9e652af333ffee
gudtldn <gudtldn1234@gmail.com>
2025-05-24 16:55:18 +0900

    feat: BodySetup 관련 소스 및 헤더 파일 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/PhysicsEngine/BodySetup.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "AggregateGeom.h"
+#include "BodySetupCore.h"
+
+
+class UBodySetup : public UBodySetupCore
+{
+    DECLARE_CLASS(UBodySetup, UBodySetupCore)
+
+public:
+    UBodySetup() = default;
+    virtual ~UBodySetup() override = default;
+
+public:
+    UPROPERTY(
+        EditAnywhere, ({ .Category = "BodySetup", .DisplayName = "Primitives" }),
+        FKAggregateGeom, AggGeom, ;
+    )
+};
