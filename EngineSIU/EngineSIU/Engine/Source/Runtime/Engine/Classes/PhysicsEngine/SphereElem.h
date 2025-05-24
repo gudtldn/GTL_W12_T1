556e71b81b4e1ae3e1e027d81b3b18e3c4b514db
gudtldn <gudtldn1234@gmail.com>
2025-05-24 16:48:33 +0900

    feat: PhysicsEngine 관련 클래스 및 파일 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/PhysicsEngine/SphereElem.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "ShapeElem.h"
+#include "UObject/ObjectMacros.h"
+
+
+struct FKSphereElem : public FKShapeElem
+{
+    DECLARE_STRUCT(FKSphereElem, FKShapeElem)
+
+public:
+    FKSphereElem() = default;
+    virtual ~FKSphereElem() override = default;
+
+    FKSphereElem(const FKSphereElem&) = default;
+    FKSphereElem& operator=(const FKSphereElem&) = default;
+    FKSphereElem(const FKSphereElem&&) = default;
+    FKSphereElem& operator=(FKSphereElem&&) = default;
+};
