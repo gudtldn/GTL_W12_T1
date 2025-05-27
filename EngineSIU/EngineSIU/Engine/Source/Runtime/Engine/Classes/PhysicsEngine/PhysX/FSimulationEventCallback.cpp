a33cec0d6c848ff029f0d8576fd80a1550487620
gudtldn <gudtldn1234@gmail.com>
2025-05-27 21:28:09 +0900

    feat: PhysX 시뮬레이션 이벤트 콜백 클래스 FSimulationEventCallback 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/PhysicsEngine/PhysX/FSimulationEventCallback.cpp
@@ -0,0 +1,26 @@
+﻿#include "FSimulationEventCallback.h"
+
+
+void FSimulationEventCallback::onConstraintBreak(physx::PxConstraintInfo* constraints, physx::PxU32 count)
+{
+}
+
+void FSimulationEventCallback::onWake(physx::PxActor** actors, physx::PxU32 count)
+{
+}
+
+void FSimulationEventCallback::onSleep(physx::PxActor** actors, physx::PxU32 count)
+{
+}
+
+void FSimulationEventCallback::onContact(const physx::PxContactPairHeader& pairHeader, const physx::PxContactPair* pairs, physx::PxU32 nbPairs)
+{
+}
+
+void FSimulationEventCallback::onTrigger(physx::PxTriggerPair* pairs, physx::PxU32 count)
+{
+}
+
+void FSimulationEventCallback::onAdvance(const physx::PxRigidBody* const* bodyBuffer, const physx::PxTransform* poseBuffer, const physx::PxU32 count)
+{
+}
